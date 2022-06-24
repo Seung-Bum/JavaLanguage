acf55402b2fe1f3aac83946f08788bbdb32f7cf3
Seung-Bum <sb910126@gmail.com>
2022-06-25 01:46:18 +0900

    readme 정리

---
+++ CollectionPrj/src/com/newlecture/app/util/ObjectList.java
@@ -0,0 +1,47 @@
+package com.newlecture.app.util;
+
+public class ObjectList {
+	
+	private Object[] nums;
+	private int current;
+	
+	public ObjectList() {
+		nums = new Object[3];
+		current = 0; 
+	}
+
+	public void add(int num) {
+		nums[current] = num;
+		current++;
+	}
+
+	public void clear() {
+//		for(int i=0; i < current; i++)
+//			nums[i] = 0;
+//		
+//		nums = new int[3];
+		// 이 방법으로 하면 기존 배열 참조가 안되면서 기존 배열은 가비지 컬렉터가 수거함
+		
+		current = 0;
+		// current의 공간이 없다고 한다면
+		// current의 값을 가지고 몇개인지 알아보고 값의 위치를 저장하기 때문에
+		// 0으로 지정해 주면 된다.
+		// c가 가르키는 위치에서 값이 변경된다.
+		
+	}
+
+	public int size() {
+		return current;
+	}
+
+	public Object get(int index) {
+		if(current <= index)
+			throw new IndexOutOfBoundsException();
+		return nums[index];
+	}
+	// current는 현재 데이터의 개수를 뜻한다.
+	// current가 3일 경우 index는 0,1,2가 된다.
+	// index가 current보다 클 수가 없고 같을수도 없다.
+	// 만약에 같거나 크다면 입력 오류 이므로 예외 발생한다.
+
+}
--- CollectionPrj/src/com/newlecture/app/util/Promgram.java
+++ CollectionPrj/src/com/newlecture/app/util/Promgram.java
@@ -3,7 +3,8 @@ package com.newlecture.app.util;
 public class Promgram {
 
 	public static void main(String[] args) {
-		IntList list = new IntList();
+		ObjectList list = new ObjectList();
+		//list.add("hello"); // int가 아니라 문자담김
 		list.add(3);
 		list.add(5);
 		int size = list.size();
@@ -14,11 +15,12 @@ public class Promgram {
 		System.out.printf("size : %d\n", size);
 		
 		list.add(7);
-		int num = list.get(0);
+		int num = (Integer)list.get(0); // Object => Integer
+		// 참조를 다른 형식으로 바꿔주기만 하면됨
+		// 꺼낼때 너는 무슨 형이니 라고 물어봐야함
+		// 범용자료 형식의 문제점
 		System.out.printf("num : %d\n", num);
 		
-//		num = list.get(1);
-		// java.lang.IndexOutOfBoundsException 발생
 	}
 
 }
