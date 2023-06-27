81723280dcbc0379ad30acfe170750174e428a9a
Seung-Bum <sb910126@gmail.com>
2023-06-27 23:16:08 +0900

    공공데이터 api 호출

--- Board/app/src/main/java/com/items/controller/RestAPIController.java
+++ Board/app/src/main/java/com/items/controller/RestAPIController.java
@@ -1,7 +1,8 @@
 package com.items.controller;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
@@ -17,15 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import org.springframework.http.*;
-import org.springframework.util.Assert;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class RestAPIController {
@@ -51,13 +44,16 @@ public class RestAPIController {
             headers.setContentType(MediaType.APPLICATION_JSON);
             headers.add("Content-Type", "application/json"); 
             headers.add("Accept-Charset", "UTF-8"); 
-            headers.set("Accept", "*/*;q=0.9"); // HTTP_ERROR 방지
+            //headers.set("Accept", "*/*;q=0.9"); // HTTP_ERROR 방지
             HttpEntity<?> entity = new HttpEntity<>(headers);                                          
          
             String frontUrl = "http://apis.data.go.kr/1360000/AirInfoService/getAirInfo?serviceKey=";
-            String backUrl = "&pageNo=1&numOfRows=10&fctm=202306120000&icaoCode=RKSI"; // icaoCode 국내공항 코드
+            String backUrl = "&pageNo=1&numOfRows=10&fctm=202306270000&icaoCode=RKSI"; // icaoCode 국내공항 코드
             
             //UriComponents uri = UriComponentsBuilder.fromHttpUrl(frontUrl+SERVICE_KEY+backUrl).build();
+
+            //String encodingKey = URLEncoder.encode(SERVICE_KEY, "UTF-8");
+            //String decodingKey = URLDecoder.decode(SERVICE_KEY, "UTF-8");           
             
             String uri = frontUrl+SERVICE_KEY+backUrl;
             
@@ -65,9 +61,6 @@ public class RestAPIController {
             
             //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
             ResponseEntity<String> resultMap = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-            //result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
-            //result.put("header", resultMap.getHeaders()); //헤더 정보 확인
-            //result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
             
             // HTTP POST 요청에 대한 응답 확인
             System.out.println("status : " + resultMap.getStatusCode());
@@ -78,17 +71,11 @@ public class RestAPIController {
             //jsonInString = mapper.writeValueAsString(resultMap.getBody());
             
         } catch (HttpClientErrorException | HttpServerErrorException e) {
-            result.put("statusCode", e.getRawStatusCode());
-            result.put("body"  , e.getStatusText());
-            System.out.println("dfdfdfdf");
-            System.out.println(e.toString());
- 
+            System.out.println(e.getRawStatusCode());
+            System.out.println(e.getStatusText()); 
         } catch (Exception e) {
-            result.put("statusCode", "999");
-            result.put("body"  , "excpetion오류");
             System.out.println(e.toString());
         }		
-		
 		return "login";
 	}
 	
